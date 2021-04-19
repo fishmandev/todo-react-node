77dae3df396bbe7b31b56de4ddf13c054fb77e49
Dmitriy Fishman <fishman.code@gmail.com>
2021-04-19 08:35:16 +0300

    Implemented todo list

---
+++ frontend/src/components/List.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+
+const ToDo = () => {
+  const [list, setList] = useState([]);
+  const [task, setTask] = useState('');
+
+  const generateId = () => {
+    if (list && list.length) {
+      return Math.max(...list.map((item) => item.id)) + 1;
+    } else {
+      return 1;
+    }
+  };
+
+  const createNewTask = () => {
+    if (!task) {
+      return; // Not valid
+    }
+    const newId = generateId();
+    const newTask = {id: newId, text: task};
+    setList([...list, newTask]);
+    setTask('');
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      createNewTask();
+    }
+  };
+
+  const handleInput = (e) => {
+    setTask(e.target.value);
+  };
+
+  const deleteTask = (id) => {
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  return (
+      <div>
+        <div>
+          {list.map((item) => {
+            return <div deleteTask={deleteTask}>{item.text}</div>;
+          })}
+        </div>
+        <div>
+          <input type="text"
+                 value={task}
+                 onChange={handleInput}
+                 onKeyPress={handleKeyPress}
+          />
+        </div>
+      </div>
+  );
+};
+
+export default ToDo;
