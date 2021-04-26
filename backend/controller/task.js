877f783abf83e9d03b1364559c428bdaf7fb9d8e
Dmitriy Fishman <fishman.code@gmail.com>
2021-04-26 13:54:45 +0300

    Added 'delete' method to the 'task' controller

--- backend/controller/task.js
+++ backend/controller/task.js
@@ -6,4 +6,12 @@ module.exports = {
       return res.json({'items': rows});
     }).catch(next);
   },
+  delete: (req, res, next) => {
+    task.delete(req.params.id).then(result => {
+      if (result === 1)
+        return res.status(204).json();
+      else
+        return res.status(404).json();
+    }).catch(next);
+  },
 };
