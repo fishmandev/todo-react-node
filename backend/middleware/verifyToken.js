b6447fc958527254ec04eeff3178e2ef1d09d6d5
Dmitriy Fishman <fishman.code@gmail.com>
2021-05-03 10:47:14 +0300

    Added 'verify token' middleware

---
+++ backend/middleware/verifyToken.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  let token = req.headers['x-access-token'];
+  let tokenSecret = process.env.JWT_TOKEN_SECRET;
+  if (!token)
+    return res.status(403).json({ error: 'Forbidden' });
+  jwt.verify(token, tokenSecret, (err, decoded) => {
+    if (err)
+      return res.status(401).json({ error: 'Unauthorized' })
+    req.userId = decoded.id;
+    next();
+  });
+}
