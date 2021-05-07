9647ed0f235bd295dbf63077203444a07ab4f4d4
Dmitriy Fishman <fishman.code@gmail.com>
2021-05-07 16:04:09 +0300

    Added the ability to get a new access token and refresh the token through the refresh token

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const user = require('../model/user');
+const refreshTokenModel = require('../model/refreshToken');
 
 module.exports = {
   getAccessToken: (req, res, next) => {
@@ -10,11 +11,50 @@ module.exports = {
       if (!user || !bcrypt.compareSync(password, user.password))
         return res.status(401).json({ erorr: 'Invalid username or password' });
 
-      let token = jwt.sign({ id: user.id }, process.env.JWT_ACCESS_TOKEN_KEY, {
+      let accessToken = jwt.sign({ id: user.id }, process.env.JWT_ACCESS_TOKEN_KEY, {
         expiresIn: 86400, // expires in 24 hours
         algorithm: 'HS512'
       });
-      res.status(200).json({ accessToken: token });
+      let currentTimeStamp = + new Date();
+      refreshTokenModel.create(user.id, currentTimeStamp).then(id => {
+        let refreshToken = jwt.sign({
+          id: user.id,
+          rid: id, // row id (PK)
+          ts: currentTimeStamp
+        }, process.env.JWT_REFRESH_TOKEN_KEY, {
+          expiresIn: 86400, // expires in 24 hours
+          algorithm: 'HS512'
+        });
+        res.status(200).json({ accessToken: accessToken, refreshToken: refreshToken });
+      }).catch(next);
     }).catch(next);
+  },
+  getToken: (req, res, next) => {
+    const refreshToken = req.body.refreshToken || '';
+    let refreshTokenSecret = process.env.JWT_REFRESH_TOKEN_KEY;
+    jwt.verify(refreshToken, refreshTokenSecret, { algorithms: ['HS512'] }, (err, decoded) => {
+      if (err)
+        return res.status(401).json({ error: 'Unauthorized' })
+      refreshTokenModel.delete(decoded.rid).then(count => {
+        if (count == 0)
+          return res.status(401).json({ error: 'Unauthorized' })
+        let accessToken = jwt.sign({ id: decoded.id }, process.env.JWT_ACCESS_TOKEN_KEY, {
+          expiresIn: 86400, // expires in 24 hours
+          algorithm: 'HS512'
+        });
+        let currentTimeStamp = + new Date();
+        refreshTokenModel.create(decoded.id, currentTimeStamp).then(id => {
+          let refreshToken = jwt.sign({
+            id: user.id,
+            rid: id, // row id (PK)
+            ts: currentTimeStamp
+          }, process.env.JWT_REFRESH_TOKEN_KEY, {
+            expiresIn: 86400, // expires in 24 hours
+            algorithm: 'HS512'
+          });
+          res.status(200).json({ accessToken: accessToken, refreshToken: refreshToken });
+        }).catch(next);
+      }).catch(next);
+    });
   }
 };
