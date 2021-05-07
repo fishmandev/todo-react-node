6a2a189d06fd384fb2319c5ae4624322b85e8d10
Dmitriy Fishman <fishman.code@gmail.com>
2021-05-07 16:05:10 +0300

    Added '/token' route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const auth = require('../controller/auth');
 
 router.post('/access_token', auth.getAccessToken);
+router.post('/token', auth.getToken);
 
 module.exports = router;
