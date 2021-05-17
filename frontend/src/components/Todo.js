3f761c4465dafcf1364aca1419b58d9fc9922dbc
Dmitriy Fishman <fishman.code@gmail.com>
2021-05-17 10:42:58 +0300

    Displaying a spinner while loading tasks

--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -7,11 +7,13 @@ import './Todo.css'
 const ToDo = () => {
   const [list, setList] = useState([]);
   const [task, setTask] = useState('');
+  const [isLoaded, setIsLoaded] = useState(false);
   const Task = useTask();
 
   useEffect(() => {
-    Task.read().then(setList);
-      // eslint-disable-next-line react-hooks/exhaustive-deps
+    setIsLoaded(false);
+    Task.read().then(setList).then(() => { setIsLoaded(true) });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const deleteTask = (id) => {
@@ -41,14 +43,18 @@ const ToDo = () => {
 
   return (
     <div className='todo'>
-      <div className='container'>
-        <CreateTask
-          value={task}
-          onCreateTaskChange={handleCreateTaskInputChange}
-          onCreateTaskEnterKeyPress={handleCreateTaskEnterKeyPress}
-        />
-        <List list={list} onDeleteTask={deleteTask} />
-      </div>
+      { isLoaded ? (
+        <div className='container'>
+          <CreateTask
+            value={task}
+            onCreateTaskChange={handleCreateTaskInputChange}
+            onCreateTaskEnterKeyPress={handleCreateTaskEnterKeyPress}
+          />
+          <List list={list} onDeleteTask={deleteTask} />
+        </div>
+      ) : (
+        <div className='loader'></div>
+      )}
     </div>
   );
 };
